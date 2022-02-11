ab2f57c891048434446bf38ab8d9272e5cd0fc3d
Saul Ticona Bejar <sticona@hotmail.com>
2022-02-10 21:33:11 -0500

    crear migracion homes

--- migrations/20220211012900_homes.js
+++ migrations/20220211012900_homes.js
@@ -37,5 +37,31 @@ exports.up = function(knex) {
 // ** EXPORTS.DOWN **
 /* Aquí colocaremos toda la logica que nos va a permitir deshacer los cambios de arriba (exports.up) */
 exports.down = function(knex) {
-  
+  return knex.schema.hasTable("homes").then(function(exists){
+    if (exists) {
+      // Si existe, borra mi tabla
+      return knex.schema.dropTable("homes");
+    }
+  })  
 };
+
+
+/* *** GUÍA DE USO RÁPIDO DE KNEX *** */
+
+// ** CREAR UNA MIGRACIÓN **
+// knex migrate:make nombreDeMiMigracion
+// Esto crea una carpeta llamada migrations, y coloca dentro el historial de migraciones 
+// como archivos .js
+
+// ** EJECUTAR UNA MIGRACIÓN SOBRE EXPORTS.UP **
+// Al ejecutar una migración sobre exports.up estamos creando o modificando la
+// tabla en la base de datos
+// Ejecutar la última migración(up): knex migrate:latest
+// Ejecutar una migración(up) en especifico: knex migrate:up nombre_archivo_migracion.js
+
+// ** EJECUTAR UNA MIGRACIÓN SOBRE EXPORTS.DOWN **
+// Al ejecutar una migración sobre exports.down estamos deshaciendo los cambios que hicimos
+// en exports.up
+// Deshacer la última migración ejecutada: knex migrate:rollback latest
+// Deshacer todos los cambios: knex migrate:rollback
+// Deshacer una migración en concreto: knex migrate:down nombre_archivo_migracion.js
