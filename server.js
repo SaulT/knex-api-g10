c41a18f3be7f6f7fff7827ee500479e449bdbfed
Saul Ticona Bejar <sticona@hotmail.com>
2022-02-10 22:16:50 -0500

    rutas con vista-controlador

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+// Importar las rutas de mi vista de Home
+const homeRoutes = require('./routes/homeRoutes');
 
 const app = express();
 
 /* Middlewares (opcional) */
-app.use(express.urlEncoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 /* Rutas */
+app.use('/api/v1', homeRoutes);
 
 /* Poner el servidor a escuchar */
 app.listen(3000, ()=> {
